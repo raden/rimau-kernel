0a8270cf78b675c5befa6a75257a7d1054214334
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2014-08-13 08:53:43 +0800

    Change compiler in build file
    Signed-off-by: Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>

--- bina-stok.sh
+++ bina-stok.sh
@@ -3,7 +3,8 @@ export ARCH=arm
 #export CROSS_COMPILE=/opt/gcc-4.8-linaro/bin/arm-cortex_a15-linux-gnueabi- 
 #export CROSS_COMPILE=/opt/gcc-4.7-linaro/bin/arm-cortex_a15-linux-gnueabi- 
 #export CROSS_COMPILE=/opt/sabermod-gcc/bin/arm-eabi-
-export CROSS_COMPILE=/opt/gcc-linaro/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1-2014.07/bin/arm-cortex_a15-linux-gnueabihf-
+#export CROSS_COMPILE=/opt/gcc-linaro/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1-2014.07/bin/arm-cortex_a15-linux-gnueabihf-
+export CROSS_COMPILE=/opt/old-linaro/arm-linux-gnueabi-linaro_4.7.4-2014.01/bin/arm-linux-gnueabi-
 #echo "Cleaning old craps..."
 #make distclean
 
